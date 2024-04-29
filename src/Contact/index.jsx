75176a93820605a02bff81836f3569e52eb15ac8
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-04-29 13:29:57 +0200

    Corrigi problemas de responsividade deste projeto

--- src/Contact/index.jsx
+++ src/Contact/index.jsx
@@ -12,20 +12,20 @@ export default function Contact({color}) {
             <h2>Contatos</h2>
             <div className="contact-container">
                 <div className="card">
-                    <a href="mailto"><MdOutlineAlternateEmail color={color}  size={30}/></a>
-                    <a href="mailto:alfacetj5@gmail.com" style={{color: color}} className="icon-contact">alfacetj5@gmail.com</a>
+                    <a href="mailto:testadormoz@gmail.com"><MdOutlineAlternateEmail color={color}  size={30}/></a>
+                    <a href="mailto:testadormoz@gmail.com"  style={{color: color}} className="icon-contact invisibilities">alfacetj5@gmail.com</a>
                 </div>
                 <div className="card">
                      <a href="tel:+258846674873" style={{color: color}} className="icon-contact"><BiSolidPhone color={color} size={30}/></a>
-                    <a href="tel:+258846674873" style={{color: color}} className="icon-contact">+258 84 667 4873</a>
+                    <a href="tel:+258846674873" style={{color: color}} className="icon-contact  invisibilities">+258 84 667 4873</a>
                 </div>
                 <div className="card">
                     <a href="https://linkedin.com/in/lucas-manuel199988463" className="icon-contact"><FaLinkedin color={color} size={30} /></a>
-                    <a href="https://linkedin.com/in/lucas-manuel199988463" className="icon-contact" style={{ color: color }}>Linkedin</a>
+                    <a href="https://linkedin.com/in/lucas-manuel199988463" className="icon-contact  invisibilities" style={{ color: color }}>Linkedin</a>
                 </div>
                 <div className="card">
                     <a href="https://github.com/Lucasmanuelmz" className="icon-contact"><FaGithub color={color} size={30} /></a>
-                    <a href="https://github.com/Lucasmanuelmz" className="icon-contact" style={{color:color}}>GitHub</a>
+                    <a href="https://github.com/Lucasmanuelmz" className="icon-contact  invisibilities" style={{color:color}}>GitHub</a>
                 </div>
             </div>
         </section>
